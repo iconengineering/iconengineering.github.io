2d95358720eb82f48fdcee8919bb2aa4617e506c
Jeremy Deischer <41126368+jeremydeischer@users.noreply.github.com>
2021-10-27 12:53:43 -0600

    Otero Drone

---
+++ project/drone/otero-tributary/app-files/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2021-10-08",
+      "name": "2021-10-08",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2922774797615695,
+        "pitch": 1.1987167881532326,
+        "fov": 1.5058557091715608
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -3.0575959494595057,
+          "pitch": 1.2840507551621538,
+          "title": "Roadway Reconstruction",
+          "text": "Outfall and Junction Structure"
+        }
+      ]
+    }
+  ],
+  "name": "Otero Tributary",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
