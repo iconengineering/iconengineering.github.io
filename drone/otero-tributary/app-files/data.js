b0e08712efc8bd144da1a16d158f170c89253bbd
jdeischer <jdeischer@gmail.com>
2022-01-29 21:47:23 -0700

    Otero initialization in Drone folder

---
+++ drone/otero-tributary/app-files/data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2021-10-08",
+      "name": "2021-10-08",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2922774797615695,
+        "pitch": 1.1987167881532326,
+        "fov": 1.5058557091715608
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+        }
+      ]
+    }
+  ],
+  "name": "Otero Tributary",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
