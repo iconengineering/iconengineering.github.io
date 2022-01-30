4d8b40e6603df99a50ededc1a2725d5bf1755178
jdeischer <41126368+jeremydeischer@users.noreply.github.com>
2022-01-29 22:28:35 -0700

    Upstream US

--- project/drone/otero-tributary/us/data.js
+++ project/drone/otero-tributary/us/data.js
@@ -2,7 +2,7 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-october-8th-2021",
-      "name": "October 8th 2021",
+      "name": "October 8th, 2021",
       "levels": [
         {
           "tileSize": 256,
@@ -33,7 +33,38 @@ var APP_DATA = {
     },
     {
       "id": "1-december-19th-2021",
-      "name": "December 19th 2021",
+      "name": "December 19th, 2021",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-january-16th-2022",
+      "name": "January 16th, 2022",
       "levels": [
         {
           "tileSize": 256,
