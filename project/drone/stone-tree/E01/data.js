3c3a194f6aeb9e5886b7e7f6d22b99d56d5cca2e
Jeremy Deischer <41126368+jeremydeischer@users.noreply.github.com>
2022-04-29 18:36:45 -0600

    Stonetree Update

--- project/drone/stone-tree/E01/data.js
+++ project/drone/stone-tree/E01/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-april-16-2022",
-      "name": "April 16, 2022",
+      "id": "0-april-16th-2022",
+      "name": "April 16th, 2022",
       "levels": [
         {
           "tileSize": 256,
@@ -24,15 +24,46 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -1.542925007861097,
+        "pitch": 1.5707963267948966,
+        "fov": 1.441990686926133
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-april-26th-2022",
+      "name": "April 26th, 2022",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9339209289270816,
+        "pitch": 1.1389957112990157,
+        "fov": 1.441990686926133
       },
       "linkHotspots": [],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Stonetree Improvements - Inlet E01",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
--- project/drone/stone-tree/W04/data.js
+++ project/drone/stone-tree/W04/data.js
@@ -1,8 +1,39 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-april-16-2022",
-      "name": "April 16, 2022",
+      "id": "0-april-16th-2022",
+      "name": "April 16th, 2022",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.0820155732462524,
+        "pitch": 1.5044575411581462,
+        "fov": 1.5254595393729649
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-april-26th-2022",
+      "name": "April 26th, 2022",
       "levels": [
         {
           "tileSize": 256,
@@ -32,7 +63,7 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Stonetree Improvements - Inlet W04",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
