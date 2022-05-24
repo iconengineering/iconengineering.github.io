6b5eac74ac4e563e5862c5ad904c339a1611645d
Jeremy Deischer <41126368+jeremydeischer@users.noreply.github.com>
2022-05-24 17:36:33 -0600

    Stonetree - W4 Inlet

--- project/drone/stone-tree/W04/data.js
+++ project/drone/stone-tree/W04/data.js
@@ -24,9 +24,9 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -1.3902645751032434,
-        "pitch": 1.2362601611026562,
-        "fov": 1.3608936558192368
+        "yaw": -1.3482075524020694,
+        "pitch": 1.2789245373395524,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
@@ -55,9 +55,9 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.8129649336192433,
-        "pitch": 1.2227020286065748,
-        "fov": 1.3608936558192368
+        "yaw": 2.2017401422523717,
+        "pitch": 1.2305809664525142,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
@@ -86,15 +86,46 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.025508658298377895,
-        "pitch": 1.0913736867478292,
-        "fov": 1.3608936558192368
+        "yaw": 0.40000063881093695,
+        "pitch": 0.9005718833147576,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-may-24th-2022",
+      "name": "May 24th, 2022",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.41932362441202287,
+        "pitch": 1.0588741532749566,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     }
   ],
-  "name": "Stonetree - W04",
+  "name": "Stonetree Improvements - W4",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
