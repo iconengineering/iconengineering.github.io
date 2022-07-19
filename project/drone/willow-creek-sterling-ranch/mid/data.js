ad25ac248f369720b8a9b240dfa4efdb5b0ccb94
jdeischer <jdeischer@iconeng.com>
2022-07-18 20:18:02 -0600

    WCR Mid Updated

--- project/drone/willow-creek-sterling-ranch/mid/data.js
+++ project/drone/willow-creek-sterling-ranch/mid/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-oct-8th-2021",
-      "name": "Oct 8th 2021",
+      "id": "0-oct-8-2021",
+      "name": "Oct 8, 2021",
       "levels": [
         {
           "tileSize": 256,
@@ -24,16 +24,16 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
+        "yaw": 2.870836958831264,
+        "pitch": 0.6008922457214734,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "1-dec-5th-2021",
-      "name": "Dec 5th 2021",
+      "id": "1-dec-5-2021",
+      "name": "Dec 5, 2021",
       "levels": [
         {
           "tileSize": 256,
@@ -55,16 +55,16 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
+        "yaw": -1.9607448626874664,
+        "pitch": 0.6165433996323717,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "2-dec-18th-2021",
-      "name": "Dec 18th 2021",
+      "id": "2-dec-18-2021",
+      "name": "Dec 18, 2021",
       "levels": [
         {
           "tileSize": 256,
@@ -86,16 +86,16 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
+        "yaw": 0.20930801993644366,
+        "pitch": 0.4765337171175581,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "3-jan-8th-2022",
-      "name": "Jan 8th 2022",
+      "id": "3-jan-8-2022",
+      "name": "Jan 8, 2022",
       "levels": [
         {
           "tileSize": 256,
@@ -117,16 +117,16 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
+        "yaw": 0.9760999006651527,
+        "pitch": 0.487564590198982,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "4-feb-20th-2022",
-      "name": "Feb 20th 2022",
+      "id": "4-feb-20-2022",
+      "name": "Feb 20, 2022",
       "levels": [
         {
           "tileSize": 256,
@@ -148,15 +148,77 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
+        "yaw": -2.0588309546896078,
+        "pitch": 0.38828673246615253,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-may-14-2022",
+      "name": "May 14, 2022",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.6544647700716943,
+        "pitch": 0.49164228375924957,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-july-16-2022",
+      "name": "July 16, 2022",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.7217508039181926,
+        "pitch": 0.41821661356138584,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     }
   ],
-  "name": "Willow Creek at Sterling Ranch - Middle",
+  "name": "Willow Creek at Sterling Ranch - Site 2",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
