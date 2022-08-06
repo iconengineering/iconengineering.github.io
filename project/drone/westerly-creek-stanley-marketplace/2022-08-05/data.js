47ca1491064c94f335e818fe200ef56d57c06a9d
jdeischer <jdeischer@iconeng.com>
2022-08-06 12:17:04 -0600

    Stanley 360s

---
+++ project/drone/westerly-creek-stanley-marketplace/2022-08-05/data.js
@@ -0,0 +1,197 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-montview-bridge",
+      "name": "Montview Bridge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.18403871873552546,
+        "pitch": 0.4967764650252775,
+        "fov": 1.3348276691322356
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pedestrian-bridge",
+      "name": "Pedestrian Bridge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.15257395983878297,
+        "pitch": 0.5827372507626372,
+        "fov": 1.3348276691322356
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-easterly-creek-confluence",
+      "name": "Easterly Creek Confluence",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.5786576159189707,
+        "pitch": 0.5161871348910125,
+        "fov": 1.3348276691322356
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-easterly-outfall",
+      "name": "Easterly Outfall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.061893393232075766,
+        "pitch": 1.054554185734382,
+        "fov": 1.3348276691322356
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-mid-2201",
+      "name": "Mid 2201",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.03249847203917611,
+        "pitch": 0.5323179828563607,
+        "fov": 1.3348276691322356
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-23rd-avenue-outfall",
+      "name": "23rd Avenue Outfall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.46925613684946477,
+        "pitch": 0.6978663755200678,
+        "fov": 1.3348276691322356
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Westerly Creek - August 5th, 2022",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ project/drone/westerly-creek-stanley-marketplace/index.md
@@ -0,0 +1,25 @@
+---
+layout: redirect
+---
+## Drainage Drone
+
+
+### Recent Videos
+
+[Drainage Drone Youtube](https://www.youtube.com/channel/UCU3xJkvm3IdILvorqWBqpwg)
+
+### Panoramic
+
+#### Otero Tributary Stream Restoration
+[Upstream](../project\drone\otero-tributary\us\index.html)
+
+[Middle Reach](../project\drone\otero-tributary\mid\index.html)
+
+[Downstream](../project\drone\otero-tributary\ds\index.html)
+
+#### Willow Creek at Sterling Ranch Stream Restoration
+[Upstream](../project\drone\willow-creek-sterling-ranch\us\index.html)
+
+[Middle](../project\drone\willow-creek-sterling-ranch\mid\index.html)
+
+[Downstream](../project\drone\willow-creek-sterling-ranch\ds\index.html)
