ddf5d4647f96076a61c5211ceb8c145afa2a2d74
jdeischer <jdeischer@iconeng.com>
2022-11-15 18:35:46 -0700

    Willow Creek Park Panoramic

---
+++ project/drone/willow-creek-park/Site3/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_0975",
+      "name": "DJI_0975",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.20706849691585383,
+        "pitch": 0.6512013016553837,
+        "fov": 1.4962687907487706
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Willow Creek Park - Site 3",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
