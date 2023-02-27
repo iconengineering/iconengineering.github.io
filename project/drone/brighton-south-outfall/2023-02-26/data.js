6b5d4923426b6139a2b032a98cc401390b46607e
jdeischer <41126368+jeremydeischer@users.noreply.github.com>
2023-02-26 17:47:50 -0700

    Brighton South Outfall

---
+++ project/drone/brighton-south-outfall/2023-02-26/data.js
@@ -0,0 +1,290 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-reach-1---downstream_dji_0137",
+      "name": "Reach 1 - Downstream_DJI_0137",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.5747993135847622,
+        "pitch": 0.6344345842204113,
+        "fov": 1.5341081613008751
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-reach-1---upstream_dji_0125",
+      "name": "Reach 1 - Upstream_DJI_0125",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.4594572456558783,
+        "pitch": 1.0060904763361247,
+        "fov": 1.5341081613008751
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-reach-2---downstream_dji_0124",
+      "name": "Reach 2 - Downstream_DJI_0124",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.014504244006214861,
+        "pitch": 0.7567488688975104,
+        "fov": 1.5341081613008751
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-reach-2---midreach_dji_0121",
+      "name": "Reach 2 - Midreach_DJI_0121",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.0804433478965514,
+        "pitch": 0.9953946997146232,
+        "fov": 1.5341081613008751
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-reach-3---downstream_dji_0119",
+      "name": "Reach 3 - Downstream_DJI_0119",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.09765532719491077,
+        "pitch": 0.7711559741298615,
+        "fov": 1.5341081613008751
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-reach-3---upstream_dji_0118",
+      "name": "Reach 3 - Upstream_DJI_0118",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.06025541465218076,
+        "pitch": 0.7711559741298508,
+        "fov": 1.5341081613008751
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-reach-4---downstream_dji_0110",
+      "name": "Reach 4 - Downstream_DJI_0110",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.0832497501621887,
+        "pitch": 1.061395055774458,
+        "fov": 1.5341081613008751
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-reach-4---midreach_dji_0109",
+      "name": "Reach 4 - Midreach_DJI_0109",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.3831735202383397,
+        "pitch": 0.5816093233990145,
+        "fov": 1.5341081613008751
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-reach-4---upstream_dji_0108",
+      "name": "Reach 4 - Upstream_DJI_0108",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.4110791346290803,
+        "pitch": 1.0248429455019803,
+        "fov": 1.5341081613008751
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Brighton South Outfall",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ project/drone/brighton-south-outfall/index.md
@@ -0,0 +1,10 @@
+---
+layout: redirect
+---
+## Brighton South Outfall
+
+### Panoramic By Location
+
+[Montview Bridge](../brighton-south-outfall\2023-02-06\index.html)
+
+
