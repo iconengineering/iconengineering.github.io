c78c71db1355cdecba474f5dfdf2cbfe302bf270
josteffens <jsteffens@iconeng.com>
2025-04-14 09:45:25 -0600

    sjcd

---
+++ project/drone/sjcd-wadsworth/380-cutbank/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sta-380-cutbank",
+      "name": "Sta 3+80 Cutbank",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.06382655516241797,
+        "pitch": 0.5129141257394814,
+        "fov": 1.3487598040662003
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SJCD Wadsworth 1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
