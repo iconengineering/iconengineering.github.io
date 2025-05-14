3b27c87057e6765c24cd0e684e6fa04c8b944612
josteffens <jsteffens@iconeng.com>
2025-05-14 11:54:25 -0600

    sulphur pano

---
+++ project/drone/sulphur-gulch/canterberry_pkwy/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-canterberry-parkway",
+      "name": "Canterberry Parkway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.13069247009448226,
+        "pitch": 0.5595426826248922,
+        "fov": 1.3487598040662003
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Sulphur- Canterberry",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
