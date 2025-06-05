c1aff282d9ec2ccb3353e74ac92e93f4332bd242
josteffens <jsteffens@iconeng.com>
2025-06-05 10:11:38 -0600

    prr panos

---
+++ project/drone/poudre-river-ranch/skyview-inlet/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-skyview-ave-inlet",
+      "name": "Skyview Ave Inlet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.17899075187735392,
+        "pitch": 1.051267070553564,
+        "fov": 1.3487598040662003
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Skyview Ave Inlet",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
