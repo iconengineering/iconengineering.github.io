4298ffbbbf8dedca20f8b3664757f9d8895bbd05
josteffens <jsteffens@iconeng.com>
2026-01-13 10:44:40 -0700

    btwc

---
+++ project/drone/btwc-diversion-annotated/data.js
@@ -0,0 +1,149 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-downstream",
+      "name": "Downstream",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 2.2614962377516834,
+        "pitch": 0.743066829969468,
+        "fov": 1.354879689972831
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.3519014028343745,
+          "pitch": 0.37231198113648567,
+          "title": "Stop Log Operable Crest",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.3829132625156566,
+          "pitch": 0.7619966213773335,
+          "title": "Concrete Low Flow Channel",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.6226470234259276,
+          "pitch": 0.3515894718310655,
+          "title": "Dam",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-upstream",
+      "name": "Upstream",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.23858974191665894,
+        "pitch": 0.6605038488617403,
+        "fov": 1.354879689972831
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.31380660687505646,
+          "pitch": 0.10817312182444105,
+          "title": "Flow Measurement",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-ditch",
+      "name": "Ditch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 1.0320756983927346,
+        "pitch": 0.7687676239786327,
+        "fov": 1.354879689972831
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.8678004578488743,
+          "pitch": 0.5355208770618862,
+          "title": "Headgate- 3 Iron Sluice Gates",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "BTWC Diversion Structure",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
