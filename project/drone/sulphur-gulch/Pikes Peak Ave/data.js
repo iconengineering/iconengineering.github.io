ffc644045aac5fff4bd502dcbb3889901825e510
ZCRICON <zromano@iconeng.com>
2026-03-29 21:56:34 -0600

    Pikes Peak Ave pano- 0123- pine gulch

---
+++ project/drone/sulphur-gulch/Pikes Peak Ave/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-june-25-2025",
+      "name": "June 25, 2025",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.8945689212650514,
+        "pitch": 0.47260211325112067,
+        "fov": 1.4990976819632245
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Pikes Peak Ave",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
